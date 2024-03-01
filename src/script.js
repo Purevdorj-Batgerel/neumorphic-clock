e0975ba1888fb3807a9e2c1d049af67768f29083
Purevdorj Batgerel <purvee.hq@gmail.com>
2024-03-01 10:18:20 -0600

    chore: update

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
-const time = {}
+const time = {};
 
-const numbers = [3, 6, 9, 12]
-const styleProperties = ['hourDegree', 'minuteDegree', 'secondDegree']
+const numbers = [3, 6, 9, 12];
+const styleProperties = ["hourDegree", "minuteDegree", "secondDegree"];
 
 const numElements = new Map();
 
-numbers.forEach(num => {
-  numElements.set(num, document.getElementById(`_${num}`))
-})
+numbers.forEach((num) => {
+  numElements.set(num, document.getElementById(`_${num}`));
+});
 
-function initialize () {
+function initialize() {
   const date = new Date();
 
   time.seconds = date.getSeconds();
@@ -23,47 +23,50 @@ function initialize () {
   setInterval(update, 1000);
 }
 
-function getNumberElement (seconds) {
+function getNumberElement(seconds) {
   switch (seconds) {
     case 0:
-      return numElements.get(12)
+      return numElements.get(12);
     case 15:
-      return numElements.get(3)
+      return numElements.get(3);
     case 30:
-      return numElements.get(6)
+      return numElements.get(6);
     case 45:
-      return numElements.get(9)
+      return numElements.get(9);
   }
 }
 
-function update () {
+function update() {
   time.seconds = (time.seconds + 1) % 60;
   time.secondDegree += 6;
   time.minuteDegree += 0.1;
-  time.hourDegree += .1 / 60;
+  time.hourDegree += 0.1 / 60;
 
   document.querySelector(".shine")?.classList.remove("shine");
-  getNumberElement(time.seconds)?.classList.add("shine")
+  getNumberElement(time.seconds)?.classList.add("shine");
 
-  styleProperties.forEach(props => {
-    document.documentElement.style.setProperty(`--${props}`, `${time[props]}deg`)
-  })
+  styleProperties.forEach((props) => {
+    document.documentElement.style.setProperty(
+      `--${props}`,
+      `${time[props]}deg`
+    );
+  });
 }
 
-function onChange () {
+function onChange() {
   if (toggle.checked) {
-    document.documentElement.classList.add('theme-transition')
-    document.documentElement.setAttribute('data-theme', 'dark')
+    document.documentElement.classList.add("theme-transition");
+    document.documentElement.setAttribute("data-theme", "dark");
     window.setTimeout(() => {
-      document.documentElement.classList.remove('theme-transition')
-    }, 1000)
+      document.documentElement.classList.remove("theme-transition");
+    }, 1000);
   } else {
-    document.documentElement.classList.add('theme-transition')
-    document.documentElement.setAttribute('data-theme', 'light')
+    document.documentElement.classList.add("theme-transition");
+    document.documentElement.setAttribute("data-theme", "light");
     window.setTimeout(() => {
-      document.documentElement.classList.remove('theme-transition')
-    }, 1000)
+      document.documentElement.classList.remove("theme-transition");
+    }, 1000);
   }
 }
 
-initialize()
+initialize();
